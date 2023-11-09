944f1173d89c07f646c7043e7f5601a3cf8c8932
kentdcrawford <Kent_Crawford@bcbst.com>
2023-11-09 16:51:35 -0500

    account for differences across systems

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,17 +1,26 @@
-local lsp_zero = require('lsp-zero')
-lsp_zero.preset('recommended')
-lsp_zero.setup()
+if is_debian() then
+  local lsp_zero = require('lsp-zero')
+  lsp_zero.preset('recommended')
+  lsp_zero.setup()
 
-lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
+  lsp_zero.on_attach(function(client, bufnr)
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({buffer = bufnr})
+  end)
+end
 
 require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {'clangd'},
-  handlers = {
-    lsp_zero.default_setup,
-  },
-})
+
+if is_debian() then
+  require('mason-lspconfig').setup({
+    ensure_installed = {},
+    handlers = {
+      lsp_zero.default_setup,
+    },
+  })
+elseif is_red_hat() then
+  require('mason-lspconfig').setup({})
+  require("lspconfig").gopls.setup {}
+  require("lspconfig").pyright.setup {}
+end
--- nvim/lua/kdc/init.lua
+++ nvim/lua/kdc/init.lua
@@ -1,2 +1,19 @@
+local function get_proc_version()
+  local f = io.open('/proc/version', 'r')
+  local s = f:read('*a')
+  f.close()
+  return s
+end
+
+function is_red_hat()
+  local s = get_proc_version()
+  return string.find(s, 'Red Hat') and true or false
+end
+
+function is_debian()
+  local s = get_proc_version()
+  return string.find(s, 'Debian') and true or false
+end
+
 require('kdc.packer')
 require('kdc.remap')
