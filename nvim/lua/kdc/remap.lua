6a5f39cb3ccea270c824223bd9b6b9596bc041ad
kentdcrawford <Kent_Crawford@bcbst.com>
2023-11-20 15:39:34 -0500

    add sort

--- nvim/lua/kdc/remap.lua
+++ nvim/lua/kdc/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = ","
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("v", "<leader>s", ":sort<CR>")
 
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
