9eb6fa5f8eefa7c4c6b645a96c5354667afa7569
Kent Crawford <kentcrawford@gmail.com>
2023-11-20 15:04:56 -0500

    updates

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- need ripgrep to get this to work.  will have to build from source
+-- -- need ripgrep to get this to work.  will have to build from source
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
--- vim.keymap.set('n', '<leader>fg', function()
---     builtin.grep_string({ search = vim.fn.input("Grepp > ") });
--- end)
+-- -- vim.keymap.set('n', '<leader>fg', function()
+-- --     builtin.grep_string({ search = vim.fn.input("Grepp > ") });
+-- -- end)
