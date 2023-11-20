3c44d5b6f66c96a6b47a90fe963017dcdcfa260f
kentdcrawford <Kent_Crawford@bcbst.com>
2023-11-20 15:39:56 -0500

    move files into place

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,2 +1,8 @@
+set -e
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
      ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+mkdir -p ~/.config/nvim/lua/kdc/
+mkdir -p ~/.config/nvim/after/plugin/
+cp -v ./lua/kdc/* ~/.config/nvim/lua/kdc/
+cp -v ./after/plugin/* ~/.config/nvim/after/plugin/
+cp -v ./init.lua ~/.config/nvim/
